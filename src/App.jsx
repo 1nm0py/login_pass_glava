d62b900d1e4081174919c9cc368b2b99ff9f5f44
Inamov Asilbek <playerallgames007@gmail.cpm>
2024-07-30 14:30:17 +0500

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from './componets/Card/card_comp.jsx';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export default function App() {
+
+  const [name, setName] = useState()
+  const [description, setDescription] = useState()
+  const [img, setImg] = useState()
+
+  let obj = {
+    name: name,
+    description: description,
+    image: img
+  }
+  const postData = (e) => {
+    axios.post('https://login-pass.onrender.com/data', obj)
+      .then(response => {
+        alert(response.status);
+      })
+      .catch(error => {
+        console.error("Error sending data: ", error);
+      })
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container>
+        <Row className='justify-content-center mt-5'>
+          <Col lg={6}>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Img URL</Form.Label>
+                <Form.Control className='input_btn' onChange={(e) => setImg(e.target.value)} type="text" placeholder="Link" />
+                <Form.Text className="text-muted">
+                  ~~~ Rasmni Link ~~~ kini qo'ying
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Card Name</Form.Label>
+                <Form.Control className='input_btn' onChange={(e) => setName(e.target.value)} type="text" placeholder="Card Name" />
+                <Form.Text className="text-muted">
+                  Nomini yozing
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Card Info</Form.Label>
+                <Form.Control className='input_btn' onChange={(e) => setDescription(e.target.value)} type="text" placeholder="Card Info" />
+                <Form.Text className="text-muted">
+                  Ma'lumotni to'liq bering
+                </Form.Text>
+              </Form.Group>
+
+              {/* if (input_btn == "") { */}
+                <Button onClick={() => postData()} >
+                  --- Enter ---
+                </Button>
+              {/* } */}
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+      <Card />
     </>
   )
 }
-
-export default App
